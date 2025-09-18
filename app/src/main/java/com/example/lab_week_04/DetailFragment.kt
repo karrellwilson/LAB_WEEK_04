8492f71b53afa126d342bbcbfcfad7928ecdd579
karrellwilson <163616212+karrellwilson@users.noreply.github.com>
2025-09-18 14:51:31 +0700

    Commit No. 1 – Navigation drawer

---
+++ app/src/main/java/com/example/lab_week_04/DetailFragment.kt
@@ -0,0 +1,43 @@
+package com.example.lab_week_04 // Pastikan package name sesuai
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class DetailFragment : Fragment() {
+    private val coffeeTitle: TextView? get() = view?.findViewById(R.id.coffee_title)
+    private val coffeeDesc: TextView? get() = view?.findViewById(R.id.coffee_desc)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val coffeeId = arguments?.getInt(ListFragment.COFFEE_ID, 0) ?: 0
+        setCoffeeData(coffeeId)
+    }
+
+    private fun setCoffeeData(id: Int) {
+        when (id) {
+            R.id.affogato -> {
+                coffeeTitle?.text = getString(R.string.affogato_title)
+                coffeeDesc?.text = getString(R.string.affogato_desc)
+            }
+            R.id.americano -> {
+                coffeeTitle?.text = getString(R.string.americano_title)
+                coffeeDesc?.text = getString(R.string.americano_desc)
+            }
+            R.id.latte -> {
+                coffeeTitle?.text = getString(R.string.latte_title)
+                coffeeDesc?.text = getString(R.string.latte_desc)
+            }
+        }
+    }
+}
